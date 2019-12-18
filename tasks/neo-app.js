3f6be2357f77bed628cea022270586c27201e679
tobiu <tobiasuhlig78@gmail.com>
2019-12-18 18:26:32 +0100

    testing

--- tasks/neo-app.js
+++ tasks/neo-app.js
@@ -2,8 +2,26 @@
 
 'use strict';
 
-const fs = require('fs');
+//const fs = require('fs');
+//const path = require('path');
+//const cp = require('child_process');
 
 console.log('Create neo.mjs app');
 
-process.exit();
+const handleExit = () => {
+    console.log('Exiting without error.');
+    process.exit();
+};
+
+const handleError = e => {
+    console.error('ERROR! An error was encountered while executing');
+    console.error(e);
+    console.log('Exiting with error.');
+    process.exit(1);
+};
+
+process.on('SIGINT', handleExit);
+process.on('uncaughtException', handleError);
+
+// Cleanup
+handleExit();
