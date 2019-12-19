6d7feedad493ead16b534ce329859a71ca8247f2
tobiu <tobiasuhlig78@gmail.com>
2019-12-19 20:57:36 +0100

    added the inquirer + more content

--- tasks/neo-app.js
+++ tasks/neo-app.js
@@ -3,11 +3,25 @@
 'use strict';
 
 const fs       = require('fs'),
+      inquirer = require('inquirer'),
       path     = require('path'),
       appName  = 'MyApp',
       lAppName = appName.toLowerCase(),
       appPath  = 'apps/' + lAppName + '/',
       folder   = 'myapp';
+
+let questions = [{
+    type   : 'input',
+    name   : 'appName',
+    message: 'Please choose a name for your neo app:',
+    default: 'MyApp'
+}, {
+    type   : 'list',
+    name   : 'theme',
+    message: 'Please choose a theme for your neo app:',
+    choices: ['neo-theme-dark', 'neo-theme-light', 'both'],
+    default: 'both'
+}];
 
 console.log('Create neo.mjs app');
 
@@ -44,7 +58,122 @@ fs.mkdir(folder, { recursive: true }, (err) => {
     ].join('\n');
 
     fs.writeFileSync(folder + '/app.mjs', appContent);
+});
 
-    // Cleanup
-    handleExit();
+inquirer.prompt(questions).then(answers => {
+    const appName  = answers['appName'],
+          lAppName = appName.toLowerCase(),
+          appPath  = 'apps/' + lAppName + '/',
+          dir      = '../apps/' + lAppName,
+          folder   = path.resolve(__dirname, dir);
+
+    fs.mkdir(folder, { recursive: true }, (err) => {
+        if (err) {
+            throw err;
+        }
+
+        const appContent = [
+            "import MainContainer from './MainContainer.mjs';",
+            "",
+            "Neo.onStart = function() {",
+            "    Neo.app({",
+            "        appPath : '" + appPath + "',",
+            "        mainView: MainContainer,",
+            "        name    : '" + appName + "'",
+            "    });",
+            "};"
+        ].join('\n');
+
+        fs.writeFileSync(folder + '/app.mjs', appContent);
+
+        const indexContent = [
+            "<!DOCTYPE HTML>",
+            "<html>",
+            "<head>",
+            '    <meta name="viewport" content="width=device-width, initial-scale=1">',
+            '    <meta charset="UTF-8">',
+            "    <title>" + appName + "</title>",
+            "</head>",
+            "<body>",
+            "    <script>",
+            "        Neo = self.Neo || {}; Neo.config = Neo.config || {};",
+            "",
+            "        Object.assign(Neo.config, {",
+            "            appPath       : '" + appPath + "app.mjs',",
+            "            basePath      : '../../',",
+            "            environment   : 'development',",
+            "            isExperimental: true",
+            "        });",
+            "    </script>",
+            "",
+            '    <script src="./node_modules/neo.mjs/src/Main.mjs" type="module"></script>',
+            "</body>",
+            "</html>",
+        ];
+
+        if (answers['theme'] !== 'both') {
+            console.log('add theme');
+            indexContent[15] += ',';
+            const themeContent = "            themes        : ['" + answers['theme'] + "']";
+            indexContent.splice(16, 0, themeContent);
+        }
+
+        fs.writeFileSync(folder + '/index.html', indexContent.join('\n'));
+
+        const mainContainerContent = [
+            "import {default as Component}    from '../../src/component/Base.mjs';",
+            "import {default as TabContainer} from '../../src/tab/Container.mjs';",
+            "import Viewport                  from '../../src/container/Viewport.mjs';",
+            "",
+            "/**",
+            " * @class " + appName + ".MainContainer",
+            " * @extends Neo.container.Viewport",
+            " */",
+            "class MainContainer extends Viewport {",
+            "    static getConfig() {return {",
+            "        className: '" + appName + ".MainContainer',",
+            "        ntype    : 'main-container',",
+            "",
+            "        autoMount: true,",
+            "        layout   : {ntype: 'fit'},",
+            "",
+            "        items: [{",
+            "            module: TabContainer,",
+            "            height: 300,",
+            "            width : 500,",
+            "            style : {flex: 'none', margin: '20px'},",
+            "",
+            "            itemDefaults: {",
+            "                module: Component,",
+            "                cls   : ['neo-examples-tab-component'],",
+            "                style : {padding: '20px'},",
+            "            },",
+            "",
+            "            items: [{",
+            "                tabButtonConfig: {",
+            "                    iconCls: 'fa fa-home',",
+            "                    text   : 'Tab 1'",
+            "                },",
+            "                vdom: {innerHTML: 'Welcome to your new Neo App.'}",
+            "            }, {",
+            "                tabButtonConfig: {",
+            "                    iconCls: 'fa fa-play-circle',",
+            "                    text   : 'Tab 2'",
+            "                },",
+            "                vdom: {innerHTML: 'Have fun creating something awesome!'}",
+            "            }]",
+            "        }]",
+            "    }}",
+            "}",
+            "",
+            "Neo.applyClassConfig(MainContainer);",
+            "",
+            "export {MainContainer as default};"
+        ].join('\n');
+
+        fs.writeFileSync(folder + '/MainContainer.mjs', mainContainerContent);
+
+        // Cleanup
+        handleExit();
+    });
 });
