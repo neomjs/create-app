8be9ef137549e9c44d87e0f60d246f24445b239f
tobiu <tobiasuhlig78@gmail.com>
2019-12-27 20:41:25 +0100

    createPackageJson

---
+++ tasks/CreatePackageJson.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+    init: function (appName, folder, fs, path, os) {
+        const packageJson = {
+            name: appName.toLowerCase(),
+            version: '0.1.0',
+            private: true,
+            scripts: {
+                "server-start": "webpack-dev-server --open",
+                "build-development": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/development/webpack.config.js",
+                "build-production": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/production/webpack.config.js",
+                "dev-css-structure": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/development/webpack.scss.config.js --env.json_file=neo.structure.json",
+                "dev-theme-dark": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/development/webpack.scss.config.js --env.json_file=theme.dark.json",
+                "dev-theme-light": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/development/webpack.scss.config.js --env.json_file=theme.light.json",
+                "prod-css-structure": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/production/webpack.scss.config.js --env.json_file=neo.structure.json",
+                "prod-theme-dark": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/production/webpack.scss.config.js --env.json_file=theme.dark.json",
+                "prod-theme-light": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/production/webpack.scss.config.js --env.json_file=theme.light.json",
+                "dev-theme-dark-no-css4": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/development/webpack.scss.config.js --env.json_file=theme.dark.noCss4.json",
+                "dev-theme-light-no-css4": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/development/webpack.scss.config.js --env.json_file=theme.light.noCss4.json",
+                "prod-theme-dark-no-css4": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/production/webpack.scss.config.js --env.json_file=theme.dark.noCss4.json",
+                "prod-theme-light-no-css4": "webpack --config ./node_modules/neo.mjs/buildScripts/webpack/production/webpack.scss.config.js --env.json_file=theme.light.noCss4.json",
+                "test": "echo \"Error: no test specified\" && exit 1"
+            },
+            dependencies: {
+                //"fibers": "^4.0.2",
+                'neo.mjs': '^1.0.17',
+                "sass": "^1.24.0"
+            }
+        };
+
+        fs.writeFileSync(
+            path.join(folder, 'package.json'),
+            JSON.stringify(packageJson, null, 4) + os.EOL
+        );
+    }
+};
