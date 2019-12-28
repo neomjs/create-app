e00f45a86f1daf1aa6d029f12ea4cfc2027f033d
tobiu <tobiasuhlig78@gmail.com>
2019-12-28 13:22:49 +0100

    added all css4 theme builds after npm -i

--- tasks/neo-app.js
+++ tasks/neo-app.js
@@ -114,6 +114,12 @@ inquirer.prompt(questions).then(answers => {
         cp.spawnSync(npmCmd, ['i'], { env: process.env, cwd: workspace, stdio: 'inherit' });
 
         cp.spawnSync(npmCmd, ['run', 'dev-css-structure'], { env: process.env, cwd: workspace, stdio: 'inherit' });
+        cp.spawnSync(npmCmd, ['run', 'dev-theme-dark'],    { env: process.env, cwd: workspace, stdio: 'inherit' });
+        cp.spawnSync(npmCmd, ['run', 'dev-theme-light'],   { env: process.env, cwd: workspace, stdio: 'inherit' });
+
+        cp.spawnSync(npmCmd, ['run', 'prod-css-structure'], { env: process.env, cwd: workspace, stdio: 'inherit' });
+        cp.spawnSync(npmCmd, ['run', 'prod-theme-dark'],    { env: process.env, cwd: workspace, stdio: 'inherit' });
+        cp.spawnSync(npmCmd, ['run', 'prod-theme-light'],   { env: process.env, cwd: workspace, stdio: 'inherit' });
 
         // Cleanup
         handleExit();
