686aa1586c27d5fa4157bd070b3b01f758e39a9b
tobiu <tobiasuhlig78@gmail.com>
2019-12-29 22:29:04 +0100

    docs app: example links

--- tasks/copyDocsApp.js
+++ tasks/copyDocsApp.js
@@ -16,8 +16,8 @@ module.exports = {
 
         fs.writeFileSync(indexPath, indexData, 'utf8');
 
-        const srcRegex = /..\/..\/..\/src\//gi;
-        const srcPath  = '../../../node_modules/neo.mjs/src/';
+        const srcRegex = /..\/..\/..\/src\//gi,
+              srcPath  = '../../../node_modules/neo.mjs/src/';
 
         function adjustPaths(cls) {
             const clsPath = path.join(docsPath, 'app/view', cls);
@@ -32,8 +32,8 @@ module.exports = {
         adjustPaths('MainContainerController.mjs');
         adjustPaths('TutorialsTreeList.mjs');
 
-        const subSrcRegex = /..\/..\/..\/..\/src\//gi;
-        const subSrcPath  = '../../../../node_modules/neo.mjs/src/';
+        const subSrcRegex = /..\/..\/..\/..\/src\//gi,
+              subSrcPath  = '../../../../node_modules/neo.mjs/src/';
 
         function adjustSubPaths(cls) {
             const clsPath = path.join(docsPath, 'app/view', cls);
@@ -47,5 +47,11 @@ module.exports = {
         adjustSubPaths('classdetails/MembersList.mjs');
         adjustSubPaths('classdetails/SourceViewComponent.mjs');
         adjustSubPaths('classdetails/TutorialComponent.mjs');
+
+        const examplesSrc   = path.join(docsPath, 'examples.json'),
+              examplesRegex = /..\/..\/..\/examples\//gi,
+              examplesPath  = '../../../node_modules/neo.mjs/examples/';
+
+        fs.writeFileSync(examplesSrc, fs.readFileSync(examplesSrc, 'utf8').replace(examplesRegex, examplesPath), 'utf8');
     }
 };
