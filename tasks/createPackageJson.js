9e9024f93f0a434de6cd039c8e31900df050cbfd
tobiu <tobiasuhlig78@gmail.com>
2019-12-29 19:58:05 +0100

    v1.0.48

--- tasks/createPackageJson.js
+++ tasks/createPackageJson.js
@@ -24,7 +24,7 @@ module.exports = {
             },
             dependencies: {
                 //"fibers": "^4.0.2",
-                'neo.mjs': '^1.0.18',
+                'neo.mjs': '^1.0.19',
                 "sass": "^1.24.0"
             }
         };
