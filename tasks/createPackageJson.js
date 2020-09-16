d2e6d495eb76041370f8f2f21c988ccb85fdcb5b
tobiu <tobiasuhlig78@gmail.com>
2020-09-16 23:02:29 +0200

    v1.1.9, neo v1.4.10

--- tasks/createPackageJson.js
+++ tasks/createPackageJson.js
@@ -19,7 +19,7 @@ module.exports = {
             },
             dependencies: {
                 //"fibers": "^4.0.2",
-                'neo.mjs': '^1.4.9',
+                'neo.mjs': '^1.4.10',
                 "sass": "^1.24.0"
             }
         };
