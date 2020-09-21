7af57e5f0e7f8001f0139664c000aee780dd8020
tobiu <tobiasuhlig78@gmail.com>
2020-09-21 12:26:29 +0200

    create all scss entry points

--- tasks/createScssEntryPoints.js
+++ tasks/createScssEntryPoints.js
@@ -55,5 +55,49 @@ module.exports = {
         ].join(os.EOL);
 
         fs.writeFileSync(path.join(scssPath, 'theme_dark.noCss4.scss'), content);
+
+        content = [
+            '@use "sass:map";',
+            '$neoMap: ();',
+            '',
+            '$useCss4Vars: true;',
+            '',
+            '@import "../../../../node_modules/neo.mjs/resources/scss/mixins/all";',
+            '@import "../../../../node_modules/neo.mjs/resources/scss/theme-dark/all";',
+            '',
+            '@import "../../../../resources/scss/theme-dark/all";',
+        ].join(os.EOL);
+
+        fs.writeFileSync(path.join(scssPath, 'theme_dark.scss'), content);
+
+        content = [
+            '@use "sass:map";',
+            '$neoMap: ();',
+            '',
+            '$useCss4Vars: false;',
+            '',
+            '@import "../../../../node_modules/neo.mjs/resources/scss/mixins/all";',
+            '@import "../../../../node_modules/neo.mjs/resources/scss/theme-light/all";',
+            '@import "../../../../node_modules/neo.mjs/resources/scss/src/all";',
+            '',
+            '@import "../../../../resources/scss/theme-light/all";',
+            '@import "../../../../resources/scss/src/all";'
+        ].join(os.EOL);
+
+        fs.writeFileSync(path.join(scssPath, 'theme_light.noCss4.scss'), content);
+
+        content = [
+            '@use "sass:map";',
+            '$neoMap: ();',
+            '',
+            '$useCss4Vars: true;',
+            '',
+            '@import "../../../../node_modules/neo.mjs/resources/scss/mixins/all";',
+            '@import "../../../../node_modules/neo.mjs/resources/scss/theme-light/all";',
+            '',
+            '@import "../../../../resources/scss/theme-light/all";',
+        ].join(os.EOL);
+
+        fs.writeFileSync(path.join(scssPath, 'theme_light.scss'), content);
     }
 };
