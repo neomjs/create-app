b2dd7a1088350646a842c6ea0b00b03273530a85
tobiu <tobiasuhlig78@gmail.com>
2020-09-21 12:05:59 +0200

    creating scss_structure.scss.mjs

--- tasks/createScssEntryPoints.js
+++ tasks/createScssEntryPoints.js
@@ -2,6 +2,15 @@
 
 module.exports = {
     init: function(workspace, fs, os, path) {
-        fs.mkdirpSync(path.join(workspace, 'buildScripts/webpack/entrypoints/scss'));
+        const scssPath = path.join(workspace, 'buildScripts/webpack/entrypoints/scss');
+        let content;
+
+        fs.mkdirpSync(scssPath);
+
+        content = [
+            "import './scss_structure.scss';"
+        ].join(os.EOL);
+
+        fs.writeFileSync(path.join(scssPath, 'scss_structure.scss.mjs'), content);
     }
 };
