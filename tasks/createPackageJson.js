fd1930e157b7e45231a9094329c41677f40ba9e9
tobiu <tobiasuhlig78@gmail.com>
2021-05-11 10:32:12 +0200

    v2.0.15 using neo v2.0.36

--- tasks/createPackageJson.js
+++ tasks/createPackageJson.js
@@ -18,7 +18,7 @@ module.exports = {
                 "test": "echo \"Error: no test specified\" && exit 1"
             },
             dependencies: {
-                "neo.mjs": "^2.0.35"
+                "neo.mjs": "^2.0.36"
             }
         };
 
