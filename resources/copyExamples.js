9f4eb195e41c9752cdf0ee9c49a1cd8f196733f0
tobiu <tobiasuhlig78@gmail.com>
2021-05-27 13:30:48 +0200

    v2.1.25 copyExamples now adjusts the neo-config.json files

--- resources/copyExamples.js
+++ resources/copyExamples.js
@@ -28,15 +28,28 @@ const isFile = fileName => {
 };
 
 const parseFolder = (folderPath, index) => {
-    let content, itemPath;
+    let content, i, itemPath, prefix;
 
     fs.readdirSync(folderPath).forEach(itemName => {
         itemPath = path.join(folderPath, itemName);
 
         if (isFile(itemPath)) {
             if (itemName === 'neo-config.json') {
-                // todo: config files
-            } else {
+                content = require(itemPath);
+                prefix  = '';
+
+                for (i=0; i < index; i++) {
+                    prefix += '../'
+                }
+
+                Object.assign(content, {
+                    appPath       : prefix + content.appPath,
+                    mainPath      : '../node_modules/neo.mjs/src/Main.mjs',
+                    workerBasePath: `${prefix}../node_modules/neo.mjs/src/worker/`
+                });
+
+                fs.writeFileSync(itemPath, JSON.stringify(content, null, 4));
+            } else if (itemName.endsWith('.mjs')) {
                 content = fs.readFileSync(itemPath, 'utf8').replace(srcRegex[index], srcPath[index]);
                 fs.writeFileSync(itemPath, content, 'utf8');
             }
