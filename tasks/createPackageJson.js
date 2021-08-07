eb82f0dfcd055012453306c79859be15f78d40ba
tobiu <tobiasuhlig78@gmail.com>
2021-08-07 16:24:17 +0200

    v2.3.0 using neo v2.3.0

--- tasks/createPackageJson.js
+++ tasks/createPackageJson.js
@@ -20,7 +20,7 @@ module.exports = {
             },
             dependencies: {
                 "fs-extra": "^10.0.0",
-                "neo.mjs": "^2.2.78"
+                "neo.mjs": "^2.3.0"
             }
         };
 
