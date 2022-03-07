ffeaac5be28d622d499a9e1aa120af58e15eba79
tobiu <tobiasuhlig78@gmail.com>
2022-03-07 22:42:30 +0100

    v3.2.4 using neo v3.2.6

--- tasks/createPackageJson.mjs
+++ tasks/createPackageJson.mjs
@@ -18,7 +18,7 @@ export default {
                 "test": "echo \"Error: no test specified\" && exit 1"
             },
             dependencies: {
-                "neo.mjs": "^3.2.3"
+                "neo.mjs": "^3.2.6"
             }
         };
 
