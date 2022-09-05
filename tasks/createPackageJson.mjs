85ae1d24aac7bedc7d0f9ff9b744e7e0f705136e
tobiu <tobiasuhlig78@gmail.com>
2022-09-05 14:49:59 +0200

    v4.2.0 using neo v4.2.0

--- tasks/createPackageJson.mjs
+++ tasks/createPackageJson.mjs
@@ -20,7 +20,7 @@ export default {
                 "watch-themes": "node ./node_modules/neo.mjs/buildScripts/watchThemes.mjs"
             },
             dependencies: {
-                "neo.mjs": "^4.1.2"
+                "neo.mjs": "^4.2.0"
             }
         };
 
