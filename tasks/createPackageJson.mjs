46db41f6d6878a3455184d7e3736308e53a0cb62
tobiu <tobiasuhlig78@gmail.com>
2024-07-31 20:14:20 +0200

    neo v6.26.0

--- tasks/createPackageJson.mjs
+++ tasks/createPackageJson.mjs
@@ -23,7 +23,7 @@ export default {
                 "watch-themes": "node ./node_modules/neo.mjs/buildScripts/watchThemes.mjs"
             },
             dependencies: {
-                "neo.mjs": "^6.22.0"
+                "neo.mjs": "^6.26.0"
             },
             devDependencies: {
                 "@fortawesome/fontawesome-free": "^6.5.2",
