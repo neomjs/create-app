863996bfc6faff8855c3052c16220d5dc7939291
tobiu <tobiasuhlig78@gmail.com>
2025-01-29 23:37:08 +0100

    neo v8.12.0

--- tasks/createPackageJson.mjs
+++ tasks/createPackageJson.mjs
@@ -23,7 +23,7 @@ export default {
                 "watch-themes": "node ./node_modules/neo.mjs/buildScripts/watchThemes.mjs"
             },
             dependencies: {
-                "neo.mjs": "^8.5.0"
+                "neo.mjs": "^8.12.0"
             },
             devDependencies: {
                 "@fortawesome/fontawesome-free": "^6.6.0",
