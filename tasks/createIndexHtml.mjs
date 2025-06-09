599d3ec02563857300f68b16d47b79f3d1c3be74
tobiu <tobiasuhlig78@gmail.com>
2025-06-09 10:14:36 +0200

    createIndexHtml.mjs: more consistent usage of quotes

--- tasks/createIndexHtml.mjs
+++ tasks/createIndexHtml.mjs
@@ -1,17 +1,17 @@
 export default {
     init(appName, folder, fs, os, path) {
         const indexContent = [
-            "<!DOCTYPE HTML>",
-            "<html>",
-            "<head>",
+            '<!DOCTYPE HTML>',
+            '<html>',
+            '<head>',
             '    <meta name="viewport" content="width=device-width, initial-scale=1">',
             '    <meta charset="UTF-8">',
-            "    <title>" + appName + "</title>",
-            "</head>",
-            "<body>",
+            '    <title>' + appName + '</title>',
+            '</head>',
+            '<body>',
             '    <script src="../../src/MicroLoader.mjs" type="module"></script>',
-            "</body>",
-            "</html>",
+            '</body>',
+            '</html>',
         ];
 
         fs.writeFileSync(path.join(folder, 'index.html'), indexContent.join(os.EOL));
