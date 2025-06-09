20e2594ea330e1da6154d8af7904c9c4865a4042
tobiu <tobiasuhlig78@gmail.com>
2025-06-09 10:08:48 +0200

    dependencies update to be in sync with the neo repo

--- tasks/createPackageJson.mjs
+++ tasks/createPackageJson.mjs
@@ -23,32 +23,34 @@ export default {
                 "watch-themes": "node ./node_modules/neo.mjs/buildScripts/watchThemes.mjs"
             },
             dependencies: {
-                "neo.mjs": "^8.42.0"
+                "neo.mjs": "^9.10.4"
             },
             devDependencies: {
-                "@fortawesome/fontawesome-free": "^6.6.0",
-                "autoprefixer": "^10.4.20",
-                "chalk": "^5.3.0",
-                "clean-webpack-plugin": "^4.0.0",
-                "commander": "^12.1.0",
-                "cssnano": "^7.0.5",
-                "envinfo": "^7.13.0",
-                "fs-extra": "^11.2.0",
-                "highlightjs-line-numbers.js": "^2.8.0",
-                "inquirer": "^10.1.8",
-                "marked": "^14.0.0",
-                "monaco-editor": "0.50.0",
-                "neo-jsdoc": "1.0.1",
-                "neo-jsdoc-x": "1.0.5",
-                "postcss": "^8.4.41",
-                "sass": "^1.77.8",
-                "siesta-lite": "5.5.2",
-                "url": "^0.11.4",
-                "webpack": "^5.93.0",
-                "webpack-cli": "^5.1.4",
-                "webpack-dev-server": "^5.0.4",
-                "webpack-hook-plugin": "^1.0.7",
-                "webpack-node-externals": "^3.0.0"
+                "@fortawesome/fontawesome-free": "^6.7.2",
+                "autoprefixer"                 : "^10.4.21",
+                "chalk"                        : "^5.4.1",
+                "clean-webpack-plugin"         : "^4.0.0",
+                "commander"                    : "^14.0.0",
+                "cssnano"                      : "^7.0.7",
+                "envinfo"                      : "^7.14.0",
+                "fs-extra"                     : "^11.3.0",
+                "highlightjs-line-numbers.js"  : "^2.9.0",
+                "html-minifier-terser"         : "^7.2.0",
+                "inquirer"                     : "^12.6.3",
+                "marked"                       : "^15.0.12",
+                "monaco-editor"                : "0.50.0",
+                "neo-jsdoc"                    : "1.0.1",
+                "neo-jsdoc-x"                  : "1.0.5",
+                "postcss"                      : "^8.5.4",
+                "sass"                         : "^1.89.1",
+                "siesta-lite"                  : "5.5.2",
+                "terser"                       : "^5.41.0",
+                "url"                          : "^0.11.4",
+                "webpack"                      : "^5.99.9",
+                "webpack-cli"                  : "^6.0.1",
+                "webpack-dev-server"           : "^5.2.2",
+                "webpack-hook-plugin"          : "^1.0.7",
+                "webpack-node-externals"       : "^3.0.0"
             }
         };
 
